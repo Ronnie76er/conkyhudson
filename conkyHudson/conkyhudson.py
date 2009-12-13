fcdc019508f3f156641bcd848e36bf4b3e4d335a
Ron Alleva <ronnie@ralleva-laptop.(none)>
2009-12-13 18:16:20 -0500

    More modifications to the conky part of the program

--- conkyHudson/conkyhudson.py
+++ conkyHudson/conkyhudson.py
@@ -2,8 +2,24 @@
 
 import sys
 
+
 def main(argv):
-    print "hi"
+    try:
+        opts, args = getopt.getopt(argv, "hs:j:t:", ["help", "server", "jobs","template"])
+    except:
+        usage(argv)
+        sys.exit(2)
+        
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+            
+        elif opt in ("-s", "--server"):
+            server = arg
+            
+        elif opt in ("-j", "--jobs"):
+            jobs = arg
     
 if __name__ == "__main__":
     main(sys.argv[1:])
